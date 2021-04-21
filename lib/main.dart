952b6d3559d385093d5aed621212cdb1f15428df
Dao Pham <daopm.dean@gmail.com>
2021-04-21 23:30:21 +0700

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:dsc_flutter_chat_app_template/screens/chat_screen.dart';
 import 'package:dsc_flutter_chat_app_template/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
